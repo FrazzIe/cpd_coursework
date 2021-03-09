878d863e8a126a6f4e4c9e3b9c7e4ff7f02fe262
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-03-09 14:30:35 +0000

    add func to check if a stack exists

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import json
 import boto3
+from botocore.exceptions import ClientError
+
+cloudFormation = boto3.client("cloudformation")
 
 def loadJSON(path):
 	obj = None
@@ -13,5 +16,32 @@ def loadJSON(path):
 		raise SystemExit
 
 	return obj
+
+#https://stackoverflow.com/questions/23019166/boto-what-is-the-best-way-to-check-if-a-cloudformation-stack-is-exists
+def stackStatus(name):
+	try:
+		data = cloudFormation.describe_stacks(StackName = name)
+	except ClientError:
+		return "CLIENT_ERROR"
+
+	return data["Stacks"][0]["StackStatus"]
+
+def stackExists(name):
+	status = stackStatus(name)
+	switch = {
+		"CLIENT_ERROR": False,
+		"CREATE_FAILED": False,
+		"ROLLBACK_FAILED": False,
+		"DELETE_COMPLETE": False
+	}
+
+	return switch.get(status, True)
+
 settings = loadJSON("settings.json")
 cfTemplate = loadJSON("template.json")
+
+if stackExists(settings["stack"]):
+	print("Stack already exists: {}".format(settings["stack"]))
+	raise SystemExit
+
+print("doesn't exist cont")
