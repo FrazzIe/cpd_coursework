4c9b3ffbb9e811b33bdf2430600db5b6d162c554
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-03-10 10:46:27 +0000

    add a formatting script to convert py scripts to in-line JSON for cloud formation

---
+++ lambda/README.md
@@ -0,0 +1,7 @@
+# Lambda
+
+This directory contains all scripts that are used in the CloudFormation template. Each script can be found in the template as in-line python.
+
+# format.py
+
+This script is used to convert .py scripts in this directory into JSON template format for CloudFormation to use
---
+++ lambda/format.py
@@ -0,0 +1,38 @@
+# Format *.py files to be used in a cloud formation template
+
+import os
+import json
+
+scriptName = os.path.basename(__file__)
+scriptDir = os.path.dirname(__file__)
+outputDir = os.path.join(scriptDir, "output")
+output = {}
+
+def getFiles():
+	py = []
+	with os.scandir(scriptDir) as files:
+		for file in files:
+			if file.name.endswith(".py") and file.name != scriptName:
+				py.append({ "path": file.path, "name": file.name })
+	return py
+
+def getFileName(name):
+	obj = os.path.splitext(name)
+	return obj[0] + ".json"
+
+def getFileStruct():
+	return { "Fn::Join": ["\n", []]	}
+
+def populateFile(lines):
+	obj = getFileStruct()
+	for line in lines:
+		obj["Fn::Join"][1].append(line.rstrip())
+
+	return obj
+
+for script in getFiles():
+	with open(script["path"], "r") as file:
+		data = populateFile(file.readlines())
+		name = getFileName(script["name"])
+		with open(os.path.join(outputDir, name), "w") as outfile:
+			json.dump(data, outfile, indent = 4)
